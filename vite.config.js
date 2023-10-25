3b2ba1fa46e1f366b146a355d5f257b8c3f6f807
yilinglee <af930831@gmail.com>
2023-10-25 16:16:27 +0800

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import {resolve} from 'path'
+
+const root = resolve(__dirname, 'src')
+const outDir = resolve(__dirname, 'docs')
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/{10.25.23}/',
+  build: {
+    outDir,
+    emptyOutDir: true
+}})
